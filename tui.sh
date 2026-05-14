7139c58dee1f01aee3c9103c4328f73bb469e050
Dr-helicopter <hashmya1@gmail.com>
2026-05-14 16:37:27 +0330

    improved displayablity

--- tui.sh
+++ tui.sh
@@ -3,64 +3,113 @@
 source ./main.sh
 
 
+bar_position=4
 
 clear_screan() {
 	printf '\e[2J'
 }
 
-print_artist() {
-	printf '\e[6H'
-	i=0
-	for art in $MUSICDIR/*
-	do
-		printf '%s > %s\n' $((i++)) "${art#$MUSICDIR/}"
-		
-	done
+
+read_artists() {
+	artists_list=("$MUSICDIR"/*)
 }
 
-print_album() {
-	printf '\e[6H'
-	i=0
-	for art in $MUSICDIR/*/*
-	do
-		printf '%s > %s\n' $((i++)) "${art#$MUSICDIR/}"
-		
-	done
+read_albums() {
+	albums_list=("$MUSICDIR"/*/*)
 }
 
+read_songs() {
+	songs_list=("$MUSICDIR"/*/*/*)
+}
+
+
+
+display_update() {
+	printf '\e[6H'
+	for i in $(seq 0 "$((${#display_list[@]}-1))")
+	do
+		printf '%s > %s\n' $i "${display_list[$i]}"
+	done
+}
 
 print_page() {
-	clear_screan
-	case $page in
-		'art') print_artist ;;
-		'alb') print_album ;;
+	case $TAB in
+		0) 
+			display_list=("${artists_list[@]#$MUSICDIR/}")
+			clear_screan
+			display_update
+			;;
+		1) 
+			display_list=("${albums_list[@]#$MUSICDIR/}")
+			clear_screan
+			display_update
+			;;
+		2) 
+			display_list=("${songs_list[@]#$MUSICDIR/}")
+			clear_screan
+			display_update
+			;;
+
 		*) true ;;
 	esac
+	echo "$TAB_BAR"
 }
 
 
+update_tab_bar(){
+	local code0="\e[${bar_fg0:=31};${bar_bg0:=104}m"
+	local code1="\e[${bar_fg1:=31};${bar_bg1:=105}m"
+	local aritst_tab=" ${code0} artist"
+	local album_tab=" ${code0} album"
+	local song_tab=" ${code0} song"
+	case $TAB in
+		0) aritst_tab=" ${code1}>artist" ;;
+		1) album_tab=" ${code1}>album" ;;
+		2) song_tab=" ${code1}>song" ;;
+	esac
 
+	TAB_BAR=$(printf "\
+\e[${bar_position}H\
+$code0%*s\r%s \
+${aritst_tab}${album_tab}${song_tab}\
+\e[m" \
+"80" "|" "" )
+}
 
 
 main() {
+	clear_screan
+	TAB=0
 	page=art
-		for ((;;)); {
+	read_artists
+	read_albums
+	read_songs
+	update_tab_bar
+	display_list=()
+	for ((;;)); {
 		print_page
 			
 		read key
 
 
-		if [[ $key = 'q' ]]; then
-			exit 0
-		fi
+		case $key in 
+			q) exit 0 ;;
+			ar)
+				TAB=0
+				update_tab_bar
+				;;
+			al)
+				TAB=1
+				update_tab_bar
 
-		if [[ $key = 'al' ]]; then
-			page=alb
-		fi
-
-		if [[ $key = 'ar' ]]; then
-			page=art
-		fi
+				;;
+			sg)
+				TAB=2
+				update_tab_bar
+				;;
+			*)
+				true
+		esac
 	}
 }
 
