c956b08ea8721ba650c07042af69fe383f5ecd05
Dr-helicopter <hashmya1@gmail.com>
2026-06-21 11:36:01 +0330

    clean ups

--- editor.sh
+++ editor.sh
@@ -3,58 +3,10 @@
 source ~/scripts/MusicMan/main.sh
 
 MM_HOME="${XDG_STATE_HOME:-$HOME/.local/state}/muicman"
-tmpfile="${MM_HOME}/tmpfile"
 
 mkdir -p "$MM_HOME"
 
 
-edit_meta() {
-	cat > "$tmpfile" << EOF
-# file ${1}
-# edit the vars as you please
-
-mmARTIST="$mmARTIST"
-mmTITLE="$mmTITLE"
-mmALBUM="$mmALBUM"
-mmTRACK="$mmTRACK"
-EOF
-	"${EDITOR:=vi}" "$tmpfile"
-}
-
-change_album_order() {
-cat > "$tmpfile" << EOF
-# album ${2} - ${1}
-# 
-
-EOF
-
-	for i in "${MUSICDIR}/${1}/${2}/"*.mp3 ; do
-		_mmGetTags "$i"
-		printf '%s\t%s\n' "${mmTRACK}" "${i#$MUSICDIR/${1}/${2}/}" >> "$tmpfile"
-	done
-
-	"${EDITOR:=vi}" "$tmpfile"
-
-	while IFS= read -r line; do
-		# Skip blank lines
-		[ -z "$line" ] && continue
-
-		# Skip comments
-		case "$line" in
-			\#*) continue ;;
-		esac
-
-IFS='	' read -r number filename <<EOF
-$line
-EOF
-		filename="${MUSICDIR}/${1}/${2}/${filename}"
-
-		if [[ -f "$filename" ]]; then
-			_mmGetTags "$filename"
-			mmChangeTrackNo "$mmARTIST" "$mmALBUM" "$mmTITLE" "$number"
-		fi
-	done < "$tmpfile"
-}
 
 
 case "$1" in
